5ea51a05d97530a6cf20e7587118483d8c5467e0
Hrushikesh Gawas <65155864+rcgawas7@users.noreply.github.com>
2020-09-18 21:59:31 +0530

    Add files via upload

--- Assignment-2/PrimeNo.java
+++ Assignment-2/PrimeNo.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 class PrimeNo{
 	public static void main (String args[])
 {
-	int b;
+	int b, flag = 0;
+	
 	
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Enter a no.");
@@ -13,13 +14,24 @@ class PrimeNo{
 {
 	System.out.println("It is not a prime no");
 }
-	else
+	 else
 {
 	for(int i=2;i<=b;i++)
 	{
 	if (a%i==0)
+{
+	System.out.println("It is not prime no");
+	flag = 1;
+	break;
+}	
+
 }
 
+
+}
+	
+{
+	if(flag==0){System.out.println("It is prime no.");}
 
 }
 
